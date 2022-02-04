352d67d5f445716c7c9dd89a25fa6bd446aad63a
yakishamo <82935423+yakishamo@users.noreply.github.com>
2022-02-04 15:35:51 +0900

    四則演算と単行演算子+,-

--- 9cc.c
+++ 9cc.c
@@ -148,6 +148,7 @@ Node *new_node_num(int val) {
 //非終端記号関数
 Node *expr(void);
 Node *mul(void);
+Node *unary(void);
 Node *primary(void);
 
 Node *expr() {
@@ -163,21 +164,28 @@ Node *expr() {
 	}
 }
 
-int count = 0;
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 
 	for(;;) {
 		if(consume('*')){
 			fprintf(stderr, "*\n");
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		}
 		if(consume('/')){
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		}
 		else
 			return node;
 	}
+}
+
+Node *unary() {
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
 }
 
 Node *primary() {
